f6fdc331ed5b902491f2ba93e84717e18e70a776
Technopig100 <technopig@hotmail.com>
2022-11-10 11:48:59 -0500

    a few tweeks

--- usr/bin/fix-pacman-databases-and-keys.sh
+++ usr/bin/fix-pacman-databases-and-keys.sh
@@ -5,24 +5,24 @@
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ###############################################################################
-
-echo "Removing the pacman databases"
-
+echo
+tput setaf 1
+echo "###############################################################################"
+echo "####        Deleting and Refreshing Pacman Keyrings and Database           ####"
+echo "###############################################################################"
+tput sgr0
+echo
 sudo rm /var/lib/pacman/sync/*
-
 echo "Removing /etc/pacman.d/gnupg files"
-
 sudo rm -r /etc/pacman.d/gnupg/*
-
 sudo pacman-key --init
-
 sudo pacman-key --populate
-
 sudo -- bash -c 'echo "keyserver hkp://keyserver.ubuntu.com:80" >> /etc/pacman.d/gnupg/gpg.conf'
-
 sudo pacman -Syy
-
-
+wait
+echo
+tput setaf 2
 echo "###############################################################################"
-echo "###                               DONE                                     ####"
+echo "###                        Thank You and Good Bye!!                        ####"
 echo "###############################################################################"
+sleep 5
